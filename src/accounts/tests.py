30669375b76491d144b7601e78fdbafaa834d80a
TolMalware <tolmachdanil@gmail.com>
2019-04-19 10:52:11 +0300

    refactor

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -7,8 +7,8 @@ class NoteModelTests(TestCase):
             self.assertTrue(user)
 
     def test_new_user_is_exist(self):
-            user = User.objects.create_user(username='not name', email='not@email.com',password="notpassword")
-            self.assertTrue(user == User.objects.last())
+        user = User.objects.create_user(username='not name', email='not@email.com', password="notpassword")
+        self.assertTrue(user == User.objects.last())
 
     def test_can_create_a_new_superuser(self):
             user = User.objects.create_superuser(username='not name', email='not@email.com',password="notpassword")
